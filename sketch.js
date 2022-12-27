503b1315261e88a94f0e3c6a79b31d88a77ba63c
mlintangp <lintangpanjerino.2002@gmail.com>
2022-12-27 21:11:28 +0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let angle = 0;
+let hand_body;
+
+function preload() {
+  hand_body = loadModel("hand_body.obj");
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight, WEBGL);
+}
+
+function draw() {
+  // background(91, 140, 204);
+  background(14, 148, 243);
+
+  noStroke();
+  // fill(14, 203, 46);
+  fill(21, 110, 46);
+  // rotateX(angle);
+  rotateY(angle);
+  // rotateZ(angle);
+  scale(1.8);
+  model(hand_body);
+
+  angle += 0.02;
+}
